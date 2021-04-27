05a509d5e5203419d46af0b8c2082aef0a51609c
Sergio Peralta <serpel.js@gmail.com>
2021-04-27 09:43:38 -0600

    included POW on math lib

--- my_math.exs
+++ my_math.exs
@@ -5,6 +5,10 @@ defmodule MyMath do
     
     def fac(0) do 1 end
     def fac(n) do n * fac(n-1) end
+    
+    def pow(b,1) do b end
+    def pow(b,e) do b * pow(e-1) end
 end
 IO.puts "Fibonacci (10): #{MyMath.fib(10)}"
 IO.puts "Factorial (3): #{MyMath.fac(3)}"
+IO.puts "Pow 2^(3): #{MyMath.pow(2,3)}"
